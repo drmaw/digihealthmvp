9ddc65c28da7b069c72793396a4ee1a5293c8424
Mahdee Al Wakil <121925483+drmaw@users.noreply.github.com>
2025-12-23 00:54:58 +0600

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,20 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+window.login = async function () {
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value;
+
+  try {
+    const userCredential =
+      await signInWithEmailAndPassword(auth, email, password);
+
+    console.log("Login success:", userCredential.user.uid);
+    window.location.href = "dashboard.html";
+
+  } catch (error) {
+    console.error("LOGIN ERROR:", error.code, error.message);
+    alert(error.message);
+  }
+};
