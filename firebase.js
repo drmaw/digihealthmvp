c58d87f0c6f17c8f58dfa092be0f8dfaeb544e64
Mahdee Al Wakil <121925483+drmaw@users.noreply.github.com>
2025-12-23 01:25:48 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,4 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
+// js/firebase.js
 const firebaseConfig = {
   apiKey: "AIzaSyBqUDMQgc57d4wWSW2auFyYCS19q8vxBU4",
   authDomain: "digihealth-65f04.firebaseapp.com",
@@ -10,5 +8,5 @@ const firebaseConfig = {
   appId: "1:704628949252:web:b38a476bd9c4259f829051"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
